a645601f1f94119c3e0c19f6e16cf2739caf9e29
Sol Mane <solmane@pm.me>
2024-05-20 21:00:10 +0200

    Add Database client programmatically through node-pg

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,25 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-app.use(express.json());
-app.get('/hello', (req, res) => {
-res.json(["Hello"]);
-});
-app.listen(process.env.PORT, () => {
-console.log("Server is listening on port %s", process.env.PORT);
-});
+const db = require("./db");
+
+async function startServer() {
+  try {
+    // Test the database connection
+    await db.pool.connect();
+    console.log("Database connected successfully");
+
+    // Register Middlewares
+    app.use(express.json());
+
+    // Start the Express server
+    app.listen(process.env.PORT, () => {
+      console.log("Server is listening on port %s", process.env.PORT);
+    });
+  } catch (err) {
+    console.error("Failed to connect to the database", err);
+    process.exit(1);
+  }
+}
+
+startServer();
