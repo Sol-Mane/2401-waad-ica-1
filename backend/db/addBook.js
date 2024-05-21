cef66ec51c05a427af0ff99043372de36e1586d4
Sol Mane <solmane@pm.me>
2024-05-21 23:21:00 +0200

    Edit addBook endpoint to reflect new db model

--- backend/db/addBook.js
+++ backend/db/addBook.js
@@ -1,15 +1,19 @@
 const db = require("./db");
 
 module.exports = async function addBook(newBook) {
+  const { rows: authors } = await db.pool.query(
+    "INSERT INTO authors(name) VALUES ($1) RETURNING *",
+    [newBook.author]
+  );
+
+  const { rows: genres } = await db.pool.query(
+    "INSERT INTO genres(name) VALUES ($1) RETURNING *",
+    [newBook.genre]
+  );
+
   const { rows } = await db.pool.query(
-    "INSERT INTO books (isbn, title, author, genre, cover_url) VALUES ($1, $2, $3, $4, $5)",
-    [
-      newBook.isbn,
-      newBook.title,
-      newBook.author,
-      newBook.genre,
-      newBook.cover_url,
-    ]
+    "INSERT INTO books (isbn, title, author_id, genre_id) VALUES ($1, $2, $3, $4)",
+    [newBook.isbn, newBook.title, authors[0].id, genres[0].id]
   );
 
   return rows;
