c0aca3360272f3fb127353ae5c46dc04316c3211
Sol Mane <solmane@pm.me>
2024-05-21 19:58:05 +0200

    Update add book function to reflect simlipified proposal

--- backend/db/addBook.js
+++ backend/db/addBook.js
@@ -2,16 +2,13 @@ const db = require("./db");
 
 module.exports = async function addBook(newBook) {
   await db.pool.query(
-    "INSERT INTO books (isbn, title, author, publisher, publisher_city, year_of_publication, genre, base64_encoded_cover) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+    "INSERT INTO books (isbn, title, author, genre, cover_url) VALUES ($1, $2, $3, $4, $5)",
     [
       newBook.isbn,
       newBook.title,
       newBook.author,
-      newBook.publisher,
-      newBook.publisher_city,
-      newBook.year_of_publication,
       newBook.genre,
-      newBook.base64_encoded_cover,
+      newBook.cover_url,
     ]
   );
 };
